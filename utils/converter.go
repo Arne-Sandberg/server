8a443f28f27d07f3b91fcb05595ac1469bbca35f
Max Heidinger <max1410@outlook.de>
2018-02-06 11:31:34 +0100

    Maybe fix slash conversion on linux

--- utils/converter.go
+++ utils/converter.go
@@ -2,11 +2,15 @@ package utils
 
 import (
 	"path/filepath"
+	"regexp"
 	"strings"
 )
 
+var multiSlashReg = regexp.MustCompile(`(\/)+`)
+
 // ConvertToSlash takes a string (most likely a path) and converts all back-slashes to normal slashes
 func ConvertToSlash(path string) string {
-	cleanPath := filepath.Clean(path)
-	return strings.Replace(cleanPath, "\\", "/", -1)
+	path = filepath.Clean(path)
+	path = strings.Replace(path, "\\", "/", -1)
+	return multiSlashReg.ReplaceAllString(path, "/")
 }
