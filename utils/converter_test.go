6c8af9446c213d22f9886dcff5ac1797ce7ac62e
Max Heidinger <max1410@outlook.de>
2018-02-22 11:06:44 +0100

    Fix converter test

--- utils/converter_test.go
+++ utils/converter_test.go
@@ -3,16 +3,31 @@ package utils
 import "testing"
 
 func TestConvertToSlash(t *testing.T) {
-	var l = map[string]string{
-		"/path/to/file":      "/path/to/file",
-		"path/to/file":       "/path/to/file",
-		"\\path/to/file":     "/path/to/file",
-		"\\path\\to\\file":   "/path/to/file",
-		"\\path\\\\to\\file": "/path/to/file",
+	type InputStruct struct {
+		Path  string
+		IsDir bool
+	}
+	var l = map[InputStruct]string{
+		{"/path/to/file", false}:      "/path/to/file",
+		{"path/to/file", false}:       "/path/to/file",
+		{"\\path/to/file", false}:     "/path/to/file",
+		{"\\path\\to\\file", false}:   "/path/to/file",
+		{"\\path\\\\to\\file", false}: "/path/to/file",
+		{"/path/to/dir/", true}:       "/path/to/dir/",
+		{"path/to/dir/", true}:        "/path/to/dir/",
+		{"\\path/to/dir\\", true}:     "/path/to/dir/",
+		{"\\path\\to\\dir/", true}:    "/path/to/dir/",
+		{"\\path\\\\to\\dir\\", true}: "/path/to/dir/",
+		{"dir/", true}:                "/dir/",
+		{"dir\\", true}:               "/dir/",
+		{"/dir", true}:                "/dir/",
+		{"\\dir/", true}:              "/dir/",
+		{"/dir\\", true}:              "/dir/",
+		{"dir", true}:                 "/dir/",
 	}
 
 	for input, expOutput := range l {
-		if output := ConvertToSlash(input); output != expOutput {
+		if output := ConvertToSlash(input.Path, input.IsDir); output != expOutput {
 			t.Errorf("Expected result %s for input %s but got: %s", expOutput, input, output)
 		}
 	}
