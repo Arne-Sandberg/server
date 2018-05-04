cb9dbfc607864e37d07bbf0c2a51f8761242112b
Max Heidinger <mail@max-heidinger.de>
2018-05-05 00:36:39 +0200

    Rebuild grpc, fix errors

--- stats/stats.go
+++ stats/stats.go
@@ -1,11 +1,13 @@
 package stats
 
 import (
-	"time"
+	"math"
 	"runtime"
-	"github.com/freecloudio/freecloud/models"
-	"github.com/freecloudio/freecloud/utils"
+	"time"
+
 	"github.com/freecloudio/freecloud/auth"
+	"github.com/freecloudio/freecloud/models"
+	"github.com/golang/protobuf/ptypes/duration"
 )
 
 var (
@@ -18,20 +20,20 @@ func Init(version string, startTime time.Time) {
 	StartTime = startTime
 }
 
-func GetSystemStats() (*models.SystemStats){
+func GetSystemStats() *models.SystemStats {
 	var m runtime.MemStats
 	runtime.ReadMemStats(&m)
 	uptime := time.Since(StartTime)
 
 	return &models.SystemStats{
-		Version: Version,
-		AllocMem: m.Alloc,
+		Version:       Version,
+		AllocMem:      m.Alloc,
 		TotalAllocMem: m.TotalAlloc,
-		SystemMem: m.Sys,
-		NumGC: m.NumGC,
-		GoVersion: runtime.Version(),
+		SystemMem:     m.Sys,
+		NumGC:         m.NumGC,
+		GoVersion:     runtime.Version(),
 		NumGoroutines: uint32(runtime.NumGoroutine()),
-		NumSessions: auth.TotalSessionCount(),
-		Uptime: &models.Duration{ Seconds: uptime.Seconds(), Nanos: int32(uptime.Nanoseconds()) },
+		NumSessions:   auth.TotalSessionCount(),
+		Uptime:        &duration.Duration{Seconds: int64(math.RoundToEven(uptime.Seconds()))},
 	}
 }
